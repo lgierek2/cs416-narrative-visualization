faa2df23a3e005e9f0f190d914cb4261cb5a0997
Lindsey <lgierek2@illinois.edu>
2024-07-15 23:13:42 -0500

    Code

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let currentScene = 0;
 
 d3.csv("data.csv").then(data => {
     data.forEach(d => {
-        d.cases = +d.cases; 
-        d.deaths = +d.deaths; 
-        d.date = new Date(d.date); 
+        d.cases = +d.cases;
+        d.deaths = +d.deaths;
+        d.date = new Date(d.date);
     });
 
-    dataScenes.push(data.map(d => d.cases)); 
-    dataScenes.push(data.map(d => d.deaths)); 
-    dataScenes.push(data.map((d, i) => ({ index: i, cases: d.cases, deaths: d.deaths }))); 
+    dataScenes.push(data.map(d => d.cases));
+    dataScenes.push(data.map(d => d.deaths));
+    dataScenes.push(data.map((d, i) => ({ index: i, cases: d.cases, deaths: d.deaths })));
 
     renderScene(currentScene);
 });
@@ -22,98 +22,45 @@ function renderScene(sceneIndex) {
 
     const data = dataScenes[sceneIndex];
 
-    if (sceneIndex < 2) {
-        const x = d3.scaleBand()
-            .domain(data.map((d, i) => i))
-            .range([0, 400])
-            .padding(0.1);
+    const x = d3.scaleBand()
+        .domain(data.map((d, i) => i))
+        .range([0, 400])
+        .padding(0.1);
 
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(data)])
-            .range([200, 0]);
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([200, 0]);
 
-        svg.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", (d, i) => x(i))
-            .attr("y", d => y(d))
-            .attr("width", x.bandwidth())
-            .attr("height", d => 200 - y(d));
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", (d, i) => x(i))
+        .attr("y", d => y(d))
+        .attr("width", x.bandwidth())
+        .attr("height", d => 200 - y(d));
 
-        svg.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + 200 + ")")
-            .call(d3.axisBottom(x));
+    svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + 200 + ")")
+        .call(d3.axisBottom(x));
 
-        svg.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y));
+    svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y));
 
-        svg.append("text")
-            .attr("x", 200)
-            .attr("y", 220)
-            .attr("text-anchor", "middle")
-            .text(sceneIndex === 0 ? "Cases" : "Deaths");
+    svg.append("text")
+        .attr("x", 200)
+        .attr("y", 220)
+        .attr("text-anchor", "middle")
+        .text(sceneIndex === 0 ? "Total Cases" : sceneIndex === 1 ? "Total Deaths" : "Counts");
 
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 10)
-            .attr("x", 0 - (200 / 2))
-            .attr("text-anchor", "middle")
-            .text(sceneIndex === 0 ? "Total Cases" : "Total Deaths");
-
-    } else {
-        const x = d3.scaleBand()
-            .domain(data.map(d => d.index))
-            .range([0, 400])
-            .padding(0.1);
-
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(data, d => Math.max(d.cases, d.deaths))])
-            .range([200, 0]);
-
-        svg.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", (d) => x(d.index))
-            .attr("y", (d) => y(d.cases))
-            .attr("width", x.bandwidth() / 2)
-            .attr("height", (d) => 200 - y(d.cases))
-            .attr("transform", (d, i) => "translate(" + (x(d.index) - x.bandwidth() / 4) + ",0)");
-
-        svg.selectAll(".barDeath")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "barDeath")
-            .attr("x", (d) => x(d.index) + x.bandwidth() / 2)
-            .attr("y", (d) => y(d.deaths))
-            .attr("width", x.bandwidth() / 2)
-            .attr("height", (d) => 200 - y(d.deaths));
-
-        svg.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + 200 + ")")
-            .call(d3.axisBottom(x));
-
-        svg.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y));
-
-        svg.append("text")
-            .attr("x", 200)
-            .attr("y", 220)
-            .attr("text-anchor", "middle")
-            .text("Index");
-
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 10)
-            .attr("x", 0 - (200 / 2))
-            .attr("text-anchor", "middle")
-            .text("Counts");
-    }
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 10)
+        .attr("x", 0 - (200 / 2))
+        .attr("text-anchor", "middle")
+        .text(sceneIndex === 0 || sceneIndex === 1 ? "Counts" : "Counts");
 }
 
 document.getElementById("nextButton").onclick = function() {
