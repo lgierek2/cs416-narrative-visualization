56ed88c8076b9fd9aee0f68a25851ab75fc6cb53
Lindsey <lgierek2@illinois.edu>
2024-07-15 23:36:59 -0500

    Code

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-let dataScenes = [];
+let data = [];
 let currentScene = 0;
 
-d3.csv("data.csv").then(data => {
-    data.forEach(d => {
-        d.cases = +d.cases;
-        d.deaths = +d.deaths;
-        d.date = new Date(d.date);
-    });
-
-    dataScenes.push(data.map(d => d.cases)); // Total cases
-    dataScenes.push([5, 15, 25, 35, 45]); // Fake data for Total Deaths
-    dataScenes.push([2, 12, 22, 32, 42]); // Fake data for Counts
+d3.csv("data.csv").then(csvData => {
+    data = csvData.map(d => ({
+        date: new Date(d.date),
+        cases: +d.cases,
+        deaths: +d.deaths
+    }));
 
     renderScene(currentScene);
 });
@@ -20,25 +16,42 @@ function renderScene(sceneIndex) {
         .attr("width", 400)
         .attr("height", 200);
 
-    const data = dataScenes[sceneIndex];
+    let sceneData;
+    let yLabel;
+
+    switch (sceneIndex) {
+        case 0:
+            sceneData = data.map(d => d.cases); // Total cases
+            yLabel = "Total Cases";
+            break;
+        case 1:
+            sceneData = data.map(d => d.deaths); // Total deaths
+            yLabel = "Total Deaths";
+            break;
+        case 2:
+            sceneData = data.map((d, i) => ({ index: i, cases: d.cases, deaths: d.deaths })); // Comparison
+            yLabel = "Counts";
+            break;
+    }
 
     const x = d3.scaleBand()
-        .domain(data.map((d, i) => i))
+        .domain(sceneData.map((_, i) => i))
         .range([0, 400])
         .padding(0.1);
 
     const y = d3.scaleLinear()
-        .domain([0, d3.max(data)])
+        .domain([0, d3.max(sceneData)])
         .range([200, 0]);
 
     svg.selectAll(".bar")
-        .data(data)
+        .data(sceneData)
         .enter().append("rect")
         .attr("class", "bar")
         .attr("x", (d, i) => x(i))
         .attr("y", d => y(d))
         .attr("width", x.bandwidth())
-        .attr("height", d => 200 - y(d));
+        .attr("height", d => 200 - y(d))
+        .attr("fill", "steelblue");
 
     svg.append("g")
         .attr("class", "axis axis--x")
@@ -53,7 +66,7 @@ function renderScene(sceneIndex) {
         .attr("x", 200)
         .attr("y", 220)
         .attr("text-anchor", "middle")
-        .text(sceneIndex === 0 ? "Total Cases" : sceneIndex === 1 ? "Total Deaths" : "Counts");
+        .text(yLabel);
 
     svg.append("text")
         .attr("transform", "rotate(-90)")
@@ -64,13 +77,13 @@ function renderScene(sceneIndex) {
 }
 
 document.getElementById("nextButton").onclick = function() {
-    currentScene = (currentScene + 1) % dataScenes.length;
+    currentScene = (currentScene + 1) % 3;
     renderScene(currentScene);
     document.getElementById("prevButton").style.display = currentScene === 0 ? "none" : "inline";
 };
 
 document.getElementById("prevButton").onclick = function() {
-    currentScene = (currentScene - 1 + dataScenes.length) % dataScenes.length;
+    currentScene = (currentScene - 1 + 3) % 3;
     renderScene(currentScene);
     document.getElementById("prevButton").style.display = currentScene === 0 ? "none" : "inline";
 };
