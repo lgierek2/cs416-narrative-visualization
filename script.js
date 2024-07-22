88682d24e57a53dc6cfcd5b3ecdbc22d7ee97706
Lindsey <lgierek2@illinois.edu>
2024-07-21 19:07:35 -0500

    Fix axis for graph 3

--- script.js
+++ script.js
@@ -202,27 +202,76 @@ function renderTotalDeaths(data) {
         );
 }
 
-    function renderComparison(data) {
-        const selectedState = d3.select("#stateFilter").property("value");
-        const filteredData = data.filter(d => d.state === selectedState);
+function renderComparison(data) {
+    const svg = d3.select("#visualization").append("svg").attr("width", 800).attr("height", 400);
+    const margin = { top: 20, right: 30, bottom: 80, left: 60 }; // Adjust bottom margin for axis labels
+    const width = +svg.attr("width") - margin.left - margin.right;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
 
-        const svg = d3.select("#visualization").append("svg").attr("width", 800).attr("height", 400);
-        const margin = { top: 20, right: 30, bottom: 40, left: 60 };
-        const width = +svg.attr("width") - margin.left - margin.right;
-        const height = +svg.attr("height") - margin.top - margin.bottom;
-        const x = d3.scaleBand()
-            .domain(filteredData.map(d => d.date))
-            .range([margin.left, width - margin.right])
-            .padding(0.1);
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(filteredData, d => Math.max(d.cases, d.deaths))])
-            .range([height - margin.bottom, margin.top]);
-        svg.append("g")
-            .attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d/%Y")));
-        svg.append("g")
-            .attr("transform", `translate(${margin.left},0)`)
-            .call(d3.axisLeft(y));
-        svg.append("text")
+    const x = d3.scaleBand()
+        .domain(data.map(d => d.date))
+        .range([margin.left, width - margin.right])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => Math.max(d.cases, d.deaths))])
+        .range([height - margin.bottom, margin.top]);
+
+    // Custom tick values for March, June, September, December
+    const tickValues = d3.timeMonths(
+        d3.timeMonth.floor(d3.min(data, d => d.date)), // Start from the beginning of the first month
+        d3.timeMonth.ceil(d3.max(data, d => d.date)) // End at the end of the last month
+    ).filter(d => [0, 3, 6, 9].includes(d.getMonth()));
+
+    const xAxis = d3.axisBottom(x)
+        .tickValues(tickValues)
+        .tickFormat(d => {
+            const formatMonth = d3.timeFormat("%b")(d); // Month abbreviation
+            const formatYear = d3.timeFormat("%y")(d); // Last two digits of the year
+            return `${formatMonth} '${formatYear}`; // Format tick label
+        });
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(xAxis)
+        .selectAll("text") // Adjust text elements
+        .attr("text-anchor", "middle") // Center the text
+        .attr("dy", "1.2em"); // Add vertical spacing
+
+    svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y));
+
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", width - margin.right)
+        .attr("y", height - margin.bottom + 50) // Adjust for label space
+        .text("Date");
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", margin.left - 50) // Adjust for label space
+        .attr("x", -margin.top)
+        .text("Count");
+
+    svg.selectAll(".bar.cases")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar cases")
+        .attr("x", d => x(d.date))
+        .attr("y", d => y(d.cases))
+        .attr("width", x.bandwidth() / 2)
+        .attr("height", d => height - margin.bottom - y(d.cases))
+        .attr("fill", "steelblue");
+
+    svg.selectAll(".bar.deaths")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar deaths")
+        .attr("x", d => x(d.date) + x.bandwidth() / 2)
+        .attr("y", d => y(d.deaths))
+        .attr("width", x.bandwidth() / 2)
+        .attr("height", d => height - margin.bottom - y(d.deaths))
+        .attr("fill", "red");
     }
 });
